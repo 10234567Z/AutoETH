7b33d05f4f3f6894783d0518451a01c5c90a3cfe
10234567Z <93607971+10234567Z@users.noreply.github.com>
2025-10-12 19:53:48 +0530

    Add ProofOfIntelligence contract with TODOs

---
+++ hardhat_contract/contracts/ProofOfIntelligence.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract ProofOfIntelligence {
+    // TODO: Implement following logic 
+    // pyth price feeder function
+    // agent prediction checker
+    // updating onchain stats 
+    // 	leaderboard
+    // 	agent-specific
+    // 	loggin
+    // 	events for frontend
+    // getter functions
+    // 	agent-specific
+    // 	leaderboard
+    // 	recentmost log
+    // 	recentmost guess
+    // 	recentmost block mined
+}
