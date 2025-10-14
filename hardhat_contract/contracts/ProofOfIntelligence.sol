122d3bae64d49e698de7f5dc840affca791787c5
10234567Z <93607971+10234567Z@users.noreply.github.com>
2025-10-14 20:56:08 +0530

    Add pyth price feed functions on contract

--- hardhat_contract/contracts/ProofOfIntelligence.sol
+++ hardhat_contract/contracts/ProofOfIntelligence.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+import "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
+
 
 contract ProofOfIntelligence {
     // TODO: Implement following logic 
@@ -19,6 +22,9 @@ contract ProofOfIntelligence {
     // 	recentmost guess
     // 	recentmost block mined
     //  mempool txs
+
+    address contractAddress = 0x4305FB66699C3B2702D4d05CF36551390A4c69C6;
+    IPyth pyth = IPyth(contractAddress);
 
     struct Agent {
         address agentAddress;
@@ -48,12 +54,19 @@ contract ProofOfIntelligence {
     mapping(uint256 => Mempool) public mempoolTxs;
     address[] public top10Agents;
 
+    error POI__NotRegisteredAgent();
+    error POI__InvalidPrediction();
+    error POI__MempoolTxNotExist();
+    error POI__PythUpdateFailed();
+    error POI__NoNewPriceData();
+
     event NewGuess(address indexed agent, bool correct, uint256 blockNumber);
     event AgentRegistered(address indexed agent, address walletAddress);
     event LeaderboardUpdated(address[] topAgents);
     event NewMempoolTx(address indexed txHash, uint256 gasPrice, uint256 blockNumber);
     event PriceUpdated(int256 newPrice, uint256 timestamp);
     event BlockMined(uint256 blockNumber, address miner);
+    event PriceFeedUpdatedOnChainPyth(int256 price);
 
     modifier onlyRegisteredAgent() {
         require(agents[msg.sender].agentAddress != address(0), "Not a registered agent");
@@ -88,9 +101,20 @@ contract ProofOfIntelligence {
         emit LeaderboardUpdated(top10Agents);
     }
 
-    function getPythPrice() external view returns (int256, uint256) {
-        // Placeholder for Pyth price fetching logic
-        return (0, block.timestamp);
+    function updatePythPrice(bytes memory priceData, int256 price) internal {
+        bytes[] memory updateData = new bytes[](1);
+        updateData[0] = priceData;
+        uint feeAmount = pyth.getUpdateFee(updateData);
+        try pyth.updatePriceFeeds{value: feeAmount}(updateData) {
+            emit PriceFeedUpdatedOnChainPyth(price);
+        } catch {
+            revert POI__PythUpdateFailed();
+        }
+    }
+
+    function readPythPrice(bytes32 priceFeed) internal view returns (int256, uint256) {
+        PythStructs.Price memory currentBasePrice = pyth.getPriceNoOlderThan(priceFeed, 0);
+        return (currentBasePrice.price, currentBasePrice.publishTime);
     }
 
     function validateMempool(uint256 mempoolId, bool isValid) internal onlyRegisteredAgent {
